9e128c56b423886b7133b38388c388ec78ab4764
Rojojun <zoloman316@gmail.com>
2024-10-24 20:13:39 +0900

    AsciiDoc #2

--- src/test/java/com/rojojun/cafekiosk/domain/order/OrderRepositoryTest.java
+++ src/test/java/com/rojojun/cafekiosk/domain/order/OrderRepositoryTest.java
@@ -7,9 +7,7 @@ import com.rojojun.cafekiosk.domain.product.ProductType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
@@ -17,7 +15,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
 @DataJpaTest
